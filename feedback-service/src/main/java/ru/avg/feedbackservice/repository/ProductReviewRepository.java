88cd5b0b87aabb76b9fa6676723c9d28af8c7745
Алексей <118848145+GlazyrinAV@users.noreply.github.com>
2024-07-24 21:29:24 +0300

    седьмой модуль

--- feedback-service/src/main/java/ru/avg/feedbackservice/repository/ProductReviewRepository.java
+++ feedback-service/src/main/java/ru/avg/feedbackservice/repository/ProductReviewRepository.java
@@ -1,12 +1,14 @@
 package ru.avg.feedbackservice.repository;
 
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import ru.avg.feedbackservice.entity.ProductReview;
 
-public interface ProductReviewRepository {
+import java.util.UUID;
 
-    Mono<ProductReview> saveReview(ProductReview productReview);
+public interface ProductReviewRepository extends ReactiveCrudRepository<ProductReview, UUID> {
 
+    @Query("{'productId' : ?0}")
     Flux<ProductReview> findAllByProductId(int productId);
 }
--- feedback-service/src/main/java/ru/avg/feedbackservice/service/DefaultProductReviewsService.java
+++ feedback-service/src/main/java/ru/avg/feedbackservice/service/DefaultProductReviewsService.java
@@ -1,6 +1,9 @@
 package ru.avg.feedbackservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,15 +18,18 @@ public class DefaultProductReviewsService implements ProductReviewsService {
 
     private final ProductReviewRepository productReviewRepository;
 
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
+
     @Override
     public Mono<ProductReview> createReview(int productId, int rating, String review) {
-        return this.productReviewRepository.saveReview(
+        return this.productReviewRepository.save(
                 new ProductReview(UUID.randomUUID(), productId, rating, review)
         );
     }
 
     @Override
     public Flux<ProductReview> findProductReviewsForProduct(int productId) {
-        return this.productReviewRepository.findAllByProductId(productId);
+        return this.reactiveMongoTemplate
+                .find(Query.query(Criteria.where("productId").is(productId)), ProductReview.class);
     }
 }
